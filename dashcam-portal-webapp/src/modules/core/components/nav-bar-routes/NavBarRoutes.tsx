7c9dbcc0c416a623c518c44bafd1cbd29ccb4b1b
Samuel Menard <samuel.menard@videotron.ca>
2024-08-11 14:42:29 -0400

    Add navbar routes

---
+++ dashcam-portal-webapp/src/modules/core/components/nav-bar-routes/NavBarRoutes.tsx
@@ -0,0 +1,25 @@
+import './NavBarRoutes.scss';
+
+function NavBarRoutes() {
+    return (
+        <ul className="nav flex-column">
+            <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                    Vehicles
+                </a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">
+                    Link
+                </a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">
+                    Link
+                </a>
+            </li>
+        </ul>
+    );
+}
+
+export default NavBarRoutes;
--- dashcam-portal-webapp/src/modules/core/components/nav-bar/NavBar.tsx
+++ dashcam-portal-webapp/src/modules/core/components/nav-bar/NavBar.tsx
@@ -1,3 +1,4 @@
+import NavBarRoutes from '../nav-bar-routes/NavBarRoutes';
 import './NavBar.scss';
 
 function NavBar() {
@@ -6,7 +7,9 @@ function NavBar() {
             <div className="nav-bar-container">
                 <div className="nav-bar-layout">
                     <div className="branding-area">Branding</div>
-                    <div className="routes-area">Routes</div>
+                    <div className="routes-area">
+                        <NavBarRoutes></NavBarRoutes>
+                    </div>
                     <div className="profile-area">Profile</div>
                 </div>
             </div>
